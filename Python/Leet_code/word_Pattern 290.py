da95d7de9e83d809d8586ffc6fbcecd0c03ddc25
Franc Anougon <franc.anougon.emerson.com>
2022-01-18 11:06:34 +0100

    Update word pattern Programm

--- Python/Leet_code/word_Pattern 290.py
+++ Python/Leet_code/word_Pattern 290.py
@@ -10,101 +10,80 @@ class Solution(object):
         """
         s=str(s)
         pattern=str(pattern)
-        stop =0
         counter=[]             # Anzahl der Character in s      / len(character)= len(counter)
         character=[]            # die vorhanden characters in s   /
         indexes=[] 
         i_indexes=[]           # Nur ein Intermediär
         iterator1=0
         words=[]
-        i_words=[]
         counter_words=[]
         indexes_word=[]
         i_indexes_word=[]
         
-        if len(pattern) >= 1 or len(pattern)<= 300 or (pattern.startswith(" ") or pattern.endswith(" ")):     # Wort-Länge überprüfen
-            if s.startswith(" ") or s.endswith(" ") or len(s) <1 or len(s)> 3000:  # S überprüfen länge, anfang und ende
-                stop=1
-            for index in s:                         # S länge überprüfen
-                if index not in string.ascii_lowercase and index !=" ":
-                    stop=1
-                    break
-            if not stop:
-                if pattern.startswith(" ") or pattern.endswith(" "):
-                    return False
-                for index in pattern: 
-                    if index not in string.ascii_lowercase:     # Beinhaltet Großbuchstabe oder Zahlen --> Stop                    
-                        break                               # Wenn Break aus wird else nicht ausgeführt 
-                else:
-                    for index in pattern:
-                        if index not in character:
-                            character.append(index)          
-                            counter.append(pattern.count(index))
-                    #resultat: character=[a,b]  und counter=[2,2]
-                    # Find the indexes of a and b in s
-                    for index1 in range(0,len(character)):
-                        # index1= "a"
-                        for index in pattern:
-                            if index== character[index1]:
-                                i_indexes.append(iterator1)
-                            iterator1 +=1
-                        indexes.append(i_indexes)
-                        iterator1 =0
-                        i_indexes=[]                 # i_indexes auf null setzen
-                    # resultat indexes=[[0,3],[1,2]]
-                    #print(character)
-                    #print(counter)
-                    #print(indexes)
 
-                    if len(s.split(" ")) != len(pattern):   # Enthählt s mehr wörter oder weniger als die Anzhal der Zeichen in pattern?
-                        #print("false")
-                        return False
-                    else: 
-                        i_words=s.split(" ")
-                        for index in i_words:
-                            if index not in words:
-                                words.append(index)
-                        # Ergebnis words=["dog", "cat"]
-                        if len(words) != len(character):    #Anzahl der einzelnen Wöter in s nicht gleich Anzahl der einzelnen Zeichen in s
-                            #print("false")
-                            return False
-                        else:
-                            for index in words: 
-                                counter_words.append(s.count(index))
-                            if counter_words !=counter:     # Wiederholungen nicht gleich
-                                #print("false")
-                                return False
-                            #result counter_words= [2,2]
-                            for index in words:
-                                s=s.replace(index, index[0])
-                                s=s.replace(" ","")
-                            #print(s)
-                            #result s="dccd"
-                            i_words= []
-                            iterator1=0
-                            for index in s: 
-                                if index not in i_words:
-                                    i_words.append(index)
-                            #print(i_words)
-                            # resul i_words=["d","c"]
-                            for index in range(0,len(i_words)):   #0 a 1
-                                for index1 in s:        # s="dccd" 
-                                    if index1== i_words[index]:
-                                        i_indexes_word.append(iterator1)
-                                    iterator1 +=1
-                                indexes_word.append(i_indexes_word)
-                                i_indexes_word=[]
-                                iterator1=0
-                            # Result [[0,3],[1,2]]
-                            if indexes_word != indexes:
-                                #print("False")
-                                return False
-                            else:
-                                #print("True")
-                                return True
+        for index in pattern:
+            if index not in character:
+                character.append(index)          
+                counter.append(pattern.count(index))
+        #resultat: character=[a,b]  und counter=[2,2]
+        # Find the indexes of a and b in s
+        for index1 in range(0,len(character)):
+            # index1= "a"
+            for index in pattern:
+                if index== character[index1]:
+                    i_indexes.append(iterator1)
+                iterator1 +=1
+            indexes.append(i_indexes)
+            iterator1 =0
+            i_indexes=[]                 # i_indexes auf null setzen
+        # resultat indexes=[[0,3],[1,2]]
+        #print(character)
+        #print(counter)
+        #print(indexes)
 
-        else: 
-            #print("Nichts machen")
+        if len(s.split(" ")) != len(pattern):   # Enthählt s mehr wörter oder weniger als die Anzhal der Zeichen in pattern?
+            #print("false S 46")
             return False
-
+        else: 
+            s=s.split(" ")
+            for index in s:
+                if index not in words:
+                    words.append(index)
+            # Ergebnis words=["dog", "cat"]
+            #print(words)
+            if len(words) != len(character):
+                #print("false S 69")
+                return False
+            else:
+                for index in words: 
+                    print("Index: " + index)
+                    counter_words.append(s.count(index))
+                if counter_words !=counter:
+                    print("false S 62")
+                    #return False
+                #result counter_words= [2,2]
+                print("words: " +str(counter_words))
+                # Result: s=["dog", "cat", "cat", "dog"]
+                # Result: words=["dog","cat"]
+                print(s)
+                iterator1=0
+                for index in range(0,len(words)):    #0 a 1     #index von s auslesen
+                    for index1 in s:
+                        # s[0]= "dog"
+                        if words[index] == index1:
+                            i_indexes_word.append(iterator1)
+                        iterator1 +=1
+                    indexes_word.append(i_indexes_word)
+                    i_indexes_word=[]
+                    iterator1=0
+                    print("indexes_word: "+ str(indexes_word))
+                if indexes_word != indexes:
+                    #print("False S 98")
+                    return False
+                else:
+                    #print("True")
+                    return True
+my_object=Solution()
+print(my_object.wordPattern("syys", "a abc abc a"))
+#my_object.wordPattern("abba", "dog cat cat dog")
 

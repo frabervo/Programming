3b9a24697636ee269c4c8eced44ad57e329f57b2
frabervo <89940634+frabervo@users.noreply.github.com>
2022-09-06 08:17:04 +0200

    Create pull_files_docker.sh

---
+++ Python/projects/to_do_list/pull_files_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -L https://raw.githubusercontent.com/frabervo/Programming/main/Python/projects/to_do_list/setup.py
+wget -L https://raw.githubusercontent.com/frabervo/Programming/main/Python/projects/to_do_list/todolist.py
+wget -L https://raw.githubusercontent.com/frabervo/Programming/main/Python/projects/to_do_list/test.py
+wget -L https://raw.githubusercontent.com/frabervo/Programming/main/Python/projects/to_do_list/start_container.sh
+wget -L https://raw.githubusercontent.com/frabervo/Programming/main/Python/projects/to_do_list/uninstall_docker.sh
+wget -L https://raw.githubusercontent.com/frabervo/Programming/main/Python/projects/to_do_list/dockerfile

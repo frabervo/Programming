ecddb78e4be1c1593f5d0cf56d8002c94dd2e0c5
frabervo <89940634+frabervo@users.noreply.github.com>
2022-09-06 08:17:28 +0200

    Update pull_files_docker.sh

--- Python/projects/to_do_list/pull_files_docker.sh
+++ Python/projects/to_do_list/pull_files_docker.sh
@@ -5,4 +5,3 @@ wget -L https://raw.githubusercontent.com/frabervo/Programming/main/Python/proje
 wget -L https://raw.githubusercontent.com/frabervo/Programming/main/Python/projects/to_do_list/test.py
 wget -L https://raw.githubusercontent.com/frabervo/Programming/main/Python/projects/to_do_list/start_container.sh
 wget -L https://raw.githubusercontent.com/frabervo/Programming/main/Python/projects/to_do_list/uninstall_docker.sh
-wget -L https://raw.githubusercontent.com/frabervo/Programming/main/Python/projects/to_do_list/dockerfile
